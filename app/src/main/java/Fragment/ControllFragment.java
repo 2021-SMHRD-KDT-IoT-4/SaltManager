bca55eca2fece3b33348996f084df62f0f66f1e5
TTOCHI <jhpark1546@gmail.com>
2021-08-23 15:12:05 +0900

    haaaaa   iiiiii

--- app/src/main/java/Fragment/ControllFragment.java
+++ app/src/main/java/Fragment/ControllFragment.java
@@ -61,6 +61,6 @@ public class ControllFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_controll2, container, false);
+        return inflater.inflate(R.layout.fragment_controll, container, false);
     }
 }
