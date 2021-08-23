eeeca847b0bf96e43de0445cac0b812190c273d8
wlghkscq <wlghkscq@naver.com>
2021-08-23 14:42:38 +0900

    하단바 메뉴 추가

--- app/src/main/java/com/example/saltmanager/MainActivity.java
+++ app/src/main/java/com/example/saltmanager/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.saltmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottom_nv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottom_nv = findViewById(R.id.bottom_nv);
+
+
+
+
     }
 }
