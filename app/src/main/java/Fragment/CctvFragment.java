c6bb506dc75191db327cd4ee4afc2c5b9445c137
wlghkscq <wlghkscq@naver.com>
2021-08-26 03:55:31 +0900

    cctv 추가중

--- app/src/main/java/Fragment/CctvFragment.java
+++ app/src/main/java/Fragment/CctvFragment.java
@@ -7,60 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.saltmanager.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CctvFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import Adapter.CameraAdapter;
+import Model.CameraVO;
+
 public class CctvFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CctvFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CctvFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CctvFragment newInstance(String param1, String param2) {
-        CctvFragment fragment = new CctvFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    ArrayList<CameraVO> data = new ArrayList<CameraVO>();
+    ListView lv_cctv;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cctv, container, false);
+        View view =inflater.inflate(R.layout.fragment_cctv, container, false);
+
+        lv_cctv = view.findViewById(R.id.lv_cctv_list);
+
+        int[] intarr = {1,2,3,4};
+        String[] stringArr = {"http://169.254.80.8:8081/", "http://169.254.80.8:8081/", "http://169.254.80.8:8081/","http://169.254.80.8:8081/"};
+        for (int i = 0; i < intarr.length; i++){
+            CameraVO vo = new CameraVO(intarr[i], stringArr[i]);
+            data.add(vo);
+        }
+
+        CameraAdapter adapter = new CameraAdapter(getContext(),R.layout.cctv_list,data);
+        lv_cctv.setAdapter(adapter);
+
+        return view;
     }
 }
